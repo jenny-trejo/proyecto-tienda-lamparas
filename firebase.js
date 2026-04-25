e46b86aec9ae7eb8c9aa7f78bd12de9faabf7b01
jenny-trejo <jennytrejo064@gmail.com>
2026-04-25 12:46:27 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -6,5 +6,6 @@ const firebaseConfig = {
   messagingSenderId: "272544835885",
   appId: "1:272544835885:web:2d87c382dbf8f665800ba4"
 };
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firebase();
+// Inicializar
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
