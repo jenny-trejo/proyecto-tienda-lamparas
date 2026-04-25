a409de88c69a14d2ade37de9725b79b64aa3e828
jenny-trejo <jennytrejo064@gmail.com>
2026-04-25 14:39:11 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+import { auth } from "./firebase.js";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const form = document.getElementById("login-form");
+const mensaje = document.getElementById("mensaje");
+const registerBtn = document.getElementById("register");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "tienda.html";
+    mensaje.textContent = "✅ Bienvenido";
+  } catch (error) {
+    mensaje.textContent = "❌ " + error.message;
+  }
+});
+
+registerBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    mensaje.textContent = "✅ Cuenta creada";
+  } catch (error) {
+    mensaje.textContent = "❌ " + error.message;
+  }
+});
 registerBtn.addEventListener("click", async () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
