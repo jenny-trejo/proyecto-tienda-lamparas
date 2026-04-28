ab4d13bd7c6b5d84e1bea7761d1d54abfd141917
jenny-trejo <jennytrejo064@gmail.com>
2026-04-28 13:59:09 -0600

    Add files via upload

--- auth.js
+++ auth.js
@@ -7,14 +7,13 @@ import {
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCAre1FtPQ1mH5sch_l15_QCUrrE6feJTs",
-    authDomain: "ventas-app-pro.firebaseapp.com",
-    projectId: "ventas-app-pro",
-    storageBucket: "ventas-app-pro.firebasestorage.app",
-    messagingSenderId: "142986289176",
-    appId: "1:142986289176:web:4906fdca2901a5b6aae36d"
+    apiKey: "AIzaSyDq96DmifjGKblR_p2fEmFDlch8Boq9Ta4",
+    authDomain: "proyecto-tienda-lamparas.firebaseapp.com",
+    projectId: "proyecto-tienda-lamparas",
+    storageBucket: "proyecto-tienda-lamparas.firebasestorage.app",
+    messagingSenderId: "272544835885",
+    appId: "1:272544835885:web:2d87c382dbf8f665800ba4"
   };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
